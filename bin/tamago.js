4e172fe2dab00b0f756be79d0103eaa7efeb182a
Alexandre Ronsaut <alex@apollonet.fr>
2019-10-12 17:24:23 +0200

    Split help into its own module

--- bin/tamago.js
+++ bin/tamago.js
@@ -4,6 +4,7 @@
 const process = require('process')
 const build = require('../lib/build')
 const init = require('../lib/init')
+const help = require('../lib/help')
 const serve = require('../lib/serve')
 
 // Let's
@@ -14,8 +15,12 @@ go()
  */
 function go () {
   const args = process.argv.slice(2)
+  const cmd = args.length ? args[0] : 'help'
   const commands = {
+    'help': help,
+    'h': help,
     'init': init,
+    'i': init,
     'build': build,
     'b': build,
     'serve': serve,
@@ -23,17 +28,5 @@ function go () {
     'watch': serve,
     'w': serve
   }
-  commands[args[0]]
-    ? commands[args[0]](args[1])
-    : help()
-}
-
-/**
- * Help.
- */
-function help () {
-  console.log('⚠️ Commands are:')
-  console.log('$ tamago init "My website"')
-  console.log('$ tamago build')
-  console.log('$ tamago serve')
+  commands[cmd](args[1])
 }
